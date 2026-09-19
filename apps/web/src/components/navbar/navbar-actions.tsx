e6b6b51e6b9bd7c88d8aec988844e7b65040bea5
Mihaela <mikaela.malanici@gmail.com>
2026-09-19 08:43:45 +0300

    Update WIP wording and comment out tour button in Navbar actions

--- apps/web/src/components/navbar/navbar-actions.tsx
+++ apps/web/src/components/navbar/navbar-actions.tsx
@@ -1,18 +1,18 @@
 import { CvDownloadLink } from "./cv-download-link";
 import { LanguageSwitcher } from "./language-switcher";
 import { MobileMenu } from "./mobile-menu";
-import { TourTriggerButton } from "./tour-trigger-button";
+// import { TourTriggerButton } from "./tour-trigger-button";
 
 export function NavbarActions() {
   return (
     <div className="flex items-center gap-3 lg:gap-4.5">
       <LanguageSwitcher />
       <div className="hidden items-center gap-4.5 lg:flex">
-        <TourTriggerButton />
+        {/* <TourTriggerButton /> */}
         <CvDownloadLink />
       </div>
       <MobileMenu>
-        <TourTriggerButton />
+        {/* <TourTriggerButton /> */}
         <CvDownloadLink />
       </MobileMenu>
     </div>
